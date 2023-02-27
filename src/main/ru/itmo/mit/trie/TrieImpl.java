e5ccdab330e36b6808f8a27280802aea8bdc8c4a
Cherniy Kot <andrei1098765432@mail.ru>
2023-02-27 18:14:44 +0300

    implemented all methods and added tests

--- src/main/ru/itmo/mit/trie/TrieImpl.java
+++ src/main/ru/itmo/mit/trie/TrieImpl.java
@@ -1,11 +1,11 @@
 package ru.itmo.mit.trie;
 
-import java.util.HashMap;
+import java.util.SortedMap;
+import java.util.TreeMap;
 
 public class TrieImpl implements Trie {
 
     Node root = new Node();
-    int size = 0;
 
     private Pair<Integer, Node> tryGetElement(String element) {
         Node t = root;
@@ -13,33 +13,16 @@ public class TrieImpl implements Trie {
         for (char c : element.toCharArray()) {
             Node t1 = t.getNext(c);
             if (t1 == null) {
-                var result = new Pair<Integer, Node>();
-                result.first = n;
-                result.second = t;
-                return result;
+                break;
             }
             t = t1;
             n++;
         }
-        var result = new Pair<Integer, Node>();
-        result.first = n;
-        result.second = t;
-        return result;
+        return new Pair<>(n, t);
     }
 
     @Override
     public boolean add(String element) {
-//        Node t = root;
-//        if (contains(element)) { // I know, i traverse it twice, but it's Java, who cares
-//            return false;
-//        }
-//        for (char c :
-//                element.toCharArray()) {
-//            t = t.AddNext(c, false);
-//        }
-//        t.isTerminal = true;
-//        return true;
-
         var t = tryGetElement(element);
         if (t.first == element.length() && t.second.isTerminal) {
             return false;
@@ -52,19 +35,17 @@ public class TrieImpl implements Trie {
             node = node.addNext(c);
         }
         node.isTerminal = true;
+
+        do {
+            node.size++;
+            node = node.parent;
+        } while (node != null);
+
         return true;
     }
 
     @Override
     public boolean contains(String element) {
-//        Node t = root;
-//        for (char c :
-//                element.toCharArray()) {
-//            t = t.GetNext(c);
-//            if(t == null){
-//                return false;
-//            }
-//        }
         var t = tryGetElement(element);
         return t.first == element.length() && t.second.isTerminal;
     }
@@ -74,6 +55,22 @@ public class TrieImpl implements Trie {
         var t = tryGetElement(element);
         if (t.first == element.length() && t.second.isTerminal) {
             t.second.isTerminal = false;
+            Node node = t.second;
+            Node node1 = node.parent;
+            while (node1 != null) {
+                node1.children.remove(node.c);
+                if (node1.children.size() == 0 && !node1.isTerminal) {
+                    node = node1;
+                    node1 = node.parent;
+                } else {
+                    break;
+                }
+            }
+
+            do {
+                node.size--;
+                node = node.parent;
+            } while (node != null);
             return true;
         }
         return false;
@@ -81,42 +78,99 @@ public class TrieImpl implements Trie {
 
     @Override
     public int size() {
-        return size;
+        return root.size;
     }
 
     @Override
     public int howManyStartsWithPrefix(String prefix) {
+        var t = tryGetElement(prefix);
+        if (t.first == prefix.length()) {
+            return t.second.size;
+        }
         return 0;
     }
 
     @Override
     public String nextString(String element, int k) {
-        return null;
-    }
-
-    private class Node {
-        public char c;
-        public boolean isTerminal;
-        HashMap<Character, Node> next = new HashMap<>();
-
-        public Node getNext(char c) {
-            return next.get(c);
-        }
-
-        public Node addNext(char c) {
-            if (next.containsKey(c)) {
-                return next.get(c);
+        if (contains(element)) {
+            k++;
+        } else {
+            if (k == 0) {
+                return null;
             }
-            size++;
-            Node t = new Node();
-            t.c = c;
-            next.put(c, t);
-            return t;
         }
+        var t = root.traverse(element, 0, k);
+
+        Node node = t.second;
+        if (node == null) {
+            return null;
+        }
+        StringBuilder result = new StringBuilder();
+        while (node != root) {
+            result.append(node.c);
+            node = node.parent;
+        }
+        return result.reverse().toString();
     }
 
     private static class Pair<T1, T2> { //why doesn't java have it???
         public T1 first;
         public T2 second;
+
+        Pair(T1 fst, T2 snd) {
+            first = fst;
+            second = snd;
+        }
+    }
+
+    private static class Node {
+        public char c;
+        public boolean isTerminal;
+        int size;
+        SortedMap<Character, Node> children = new TreeMap<>();
+        Node parent;
+
+        public Node getNext(char c) {
+            return children.get(c);
+        }
+
+        public Node addNext(char c) {
+            if (children.containsKey(c)) {
+                return children.get(c);
+            }
+            Node t = new Node();
+            t.c = c;
+            t.parent = this;
+            children.put(c, t);
+            return t;
+        }
+
+        private Pair<Integer, Node> traverse(String element, int pos, int k) {
+            if (isTerminal) k--;
+            if (k == 0) {
+                return new Pair<>(k, this);
+            }
+            char curr = 0;
+            boolean flag = pos < element.length();
+            if (flag) {
+                curr = element.charAt(pos);
+            }
+            for (var entry : children.entrySet()) {
+                if (flag) {
+                    if (entry.getKey() != curr) {
+                        continue;
+                    }
+                    flag = false;
+                }
+
+                var t = entry.getValue().traverse(element, pos + 1, k);
+                pos = element.length();
+                if (t.first == 0) {
+                    return t;
+                }
+                k = t.first;
+            }
+            return new Pair<>(k, null);
+        }
     }
 }
--- src/test/ru/itmo/mit/trie/TrieTests.java
+++ src/test/ru/itmo/mit/trie/TrieTests.java
@@ -8,40 +8,41 @@ import static org.junit.jupiter.api.Assertions.*;
 public class TrieTests {
 
     @Test
-    public void testBasic(){
+    public void testBasic() {
         Trie t = new TrieImpl();
         assertEquals(0, t.size());
-        assertTrue( t.add("hello"));
+        assertTrue(t.add("hello"));
         assertTrue(t.contains("hello"));
         assertFalse(t.contains("world"));
-        assertEquals(5,t.size());
+        assertEquals(1, t.size());
         assertFalse(t.remove("world"));
         assertTrue(t.remove("hello"));
         assertFalse(t.contains("hello"));
+        assertEquals(0, t.size());
     }
 
     @Test
-    public void testAddContainsSize(){
+    public void testAddContainsSize() {
         Trie t = new TrieImpl();
-        assertEquals(0,t.size());
+        assertEquals(0, t.size());
 
-        String testString="Random string";
+        String testString = "Random string";
         assertTrue(t.add(testString));
-        assertEquals(testString.length(),t.size());
+        assertEquals(1, t.size());
 
-        for (int i=0;i<testString.length()-1;i++){
-            assertFalse(t.contains(testString.substring(0,i)));
+        for (int i = 0; i < testString.length() - 1; i++) {
+            assertFalse(t.contains(testString.substring(0, i)));
         }
         assertTrue(t.contains(testString));
 
         assertFalse(t.add(testString));
-        assertEquals(testString.length(),t.size());
+        assertEquals(1, t.size());
 
-        String anotherTestString="Random string but better";
+        String anotherTestString = "Random string but better";
         assertTrue(t.add(anotherTestString));
 
-        for (int i=0;i<testString.length()-1;i++){
-            assertFalse(t.contains(testString.substring(0,i)));
+        for (int i = 0; i < testString.length() - 1; i++) {
+            assertFalse(t.contains(testString.substring(0, i)));
         }
         assertTrue(t.contains(testString));
         assertTrue(t.contains(anotherTestString));
@@ -53,45 +54,104 @@ public class TrieTests {
         assertTrue(t.contains(testString));
         assertTrue(t.contains(anotherTestString));
 
-        assertEquals(anotherTestString.length(),t.size());
+        assertEquals(3, t.size());
 
-        String branchingTestString="Random, so random";
+        String branchingTestString = "Random, so random";
         assertTrue(t.add(branchingTestString));
         assertTrue(t.contains(branchingTestString));
         assertTrue(t.contains(smallerTestString));
         assertTrue(t.contains(anotherTestString));
-        assertEquals(anotherTestString.length()+(branchingTestString.length()-smallerTestString.length()),t.size());
+        assertEquals(4, t.size());
     }
 
     @Test
-    public void testRemove(){
+    public void testRemove() {
         Trie t = new TrieImpl();
-        assertEquals(0,t.size());
+        assertEquals(0, t.size());
 
-        String testString="Hola, mundo!";
+        String testString = "Hola, mundo!";
         assertTrue(t.add(testString));
         assertTrue(t.contains(testString));
+        assertEquals(1, t.size());
 
         assertTrue(t.remove(testString));
         assertFalse(t.remove(testString));
+        assertEquals(0, t.size());
 
         assertFalse(t.contains(testString));
         assertTrue(t.add(testString));
+        assertEquals(1, t.size());
 
-        String anotherTestString="Hola, como estas?";
+        String anotherTestString = "Hola, como estas?";
 
         assertTrue(t.add(anotherTestString));
         assertTrue(t.contains(anotherTestString));
         assertTrue(t.contains(testString));
+        assertEquals(2, t.size());
 
         assertTrue(t.remove(testString));
         assertFalse(t.contains(testString));
+        assertEquals(1, t.size());
 
         assertTrue(t.contains(anotherTestString));
         assertTrue(t.remove(anotherTestString));
         assertFalse(t.contains(anotherTestString));
+        assertEquals(0, t.size());
 
         assertFalse(t.remove(anotherTestString));
         assertFalse(t.remove(testString));
+        assertEquals(0, t.size());
     }
+
+    @Test
+    public void testHowManyStartsWithPrefix() {
+        Trie t = new TrieImpl();
+        assertEquals(0, t.size());
+
+        String testString1 = "Hello, world!";
+        String testString2 = "Hello, hi!";
+        String testString3 = "Hello, why!";
+        String testString4 = "Hello, goodbye!";
+
+        t.add(testString1);
+        t.add(testString2);
+        t.add(testString3);
+        t.add(testString4);
+        assertEquals(4, t.howManyStartsWithPrefix("Hello"));
+
+        String testString5 = "Hello";
+        t.add(testString5);
+        assertEquals(5, t.howManyStartsWithPrefix("Hello"));
+
+        String testString6 = "No Hello, world!";
+        t.add(testString6);
+        assertEquals(5, t.howManyStartsWithPrefix("Hello"));
+    }
+
+    @Test
+    public void testNextString() {
+        Trie t = new TrieImpl();
+        assertEquals(0, t.size());
+
+        String testString1 = "Hello, world!";
+        String testString2 = "Hello, hi!";
+        String testString3 = "Hello, why!";
+        String testString4 = "Hello, goodbye!";
+
+        t.add(testString1);
+        t.add(testString2);
+        t.add(testString3);
+        t.add(testString4);
+
+        assertEquals(testString4, t.nextString(testString4, 0));
+        assertEquals(testString2, t.nextString(testString4, 1));
+        assertEquals(testString3, t.nextString(testString4, 2));
+        assertEquals(testString1, t.nextString(testString4, 3));
+        assertNull(t.nextString(testString4, 4));
+
+        assertEquals(testString4, t.nextString("Hello", 1));
+        assertNull(t.nextString("Hello", 0));
+        assertNull(t.nextString("ZZZ", 1));
+    }
+
 }
