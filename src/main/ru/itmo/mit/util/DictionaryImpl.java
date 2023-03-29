294d8a1b230565f536aedfd5de1fcc8e815baaa6
Андрей Егорычев <thelastmind@yandex.ru>
2023-03-30 02:17:32 +0300

    implemented dict and basic tests

---
+++ src/main/ru/itmo/mit/util/DictionaryImpl.java
@@ -0,0 +1,269 @@
+package ru.itmo.mit.util;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@SuppressWarnings("SimplifyStreamApiCallChains")
+public class DictionaryImpl<K, V> implements Dictionary<K, V> {
+
+    public final double DEFAULT_LOAD_FACTOR = 0.7;
+    private double loadFactor = DEFAULT_LOAD_FACTOR;
+    private int elementCount = 0;
+
+    public final int DEFAULT_BUCKET_COUNT = 256;
+    private int initialBucketCount = DEFAULT_BUCKET_COUNT;
+    private int bucketCount = DEFAULT_BUCKET_COUNT;
+
+    private List<ArrayList<Entry<K, V>>> buckets;
+
+    DictionaryImpl() {
+        rehash(bucketCount);
+    }
+
+    DictionaryImpl(double loadFactor) {
+        this.loadFactor = loadFactor;
+        rehash(bucketCount);
+    }
+
+    DictionaryImpl(int bucketCount) {
+        this.bucketCount = bucketCount;
+        initialBucketCount = bucketCount;
+        rehash(bucketCount);
+    }
+
+    DictionaryImpl(double loadFactor, int bucketCount) {
+        this.loadFactor = loadFactor;
+        this.bucketCount = bucketCount;
+        initialBucketCount = bucketCount;
+        rehash(bucketCount);
+    }
+
+    private void rehash(int newBucketCount) {
+        var newBuckets = Stream.generate(() -> (ArrayList<Entry<K, V>>) null).limit(newBucketCount).collect(Collectors.toList());
+        if (buckets == null) {
+            buckets = newBuckets;
+            return;
+        }
+
+        for (var oldBucket : buckets) {
+            if (oldBucket == null) continue;
+            for (var entry : oldBucket) {
+                var bucketIdx = getBucketIdx(entry.getKey());
+                var bucket = newBuckets.get(bucketIdx);
+                if (bucket == null) {
+                    bucket = new ArrayList<>();
+                    newBuckets.set(bucketIdx, bucket);
+                }
+                bucket.add(entry);
+            }
+        }
+
+        buckets = newBuckets;
+    }
+
+    private double getLoad() {
+        return (double) elementCount / bucketCount;
+    }
+
+    private void rehashIfNeeded() {
+        var load = getLoad();
+        if (load < loadFactor / 3 && bucketCount > initialBucketCount) {
+            rehash(bucketCount / 2);
+        } else if (load > loadFactor) {
+            rehash(bucketCount * 2);
+        }
+    }
+
+    private int getBucketIdx(Object o) {
+        if (o == null) return 0;
+        var result = o.hashCode() % bucketCount;
+        if (result < 0) result += bucketCount;
+        return result;
+    }
+
+    @Override
+    public int size() {
+        return elementCount;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return elementCount == 0;
+    }
+
+    @Override
+    public boolean containsKey(Object key) {
+        var bucket = buckets.get(getBucketIdx(key));
+        if (bucket == null) return false;
+        for (var entry : bucket) {
+            if (Objects.equals(entry.getKey(), key)) return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean containsValue(Object value) {
+        for (var bucket : buckets) {
+            if (bucket == null) continue;
+            for (var entry : bucket) {
+                if (Objects.equals(entry.getValue(), value)) return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public V get(Object key) {
+        var bucket = buckets.get(getBucketIdx(key));
+        if (bucket == null) return null;
+        for (var entry : bucket) {
+            if (Objects.equals(entry.getKey(), key)) return entry.getValue();
+        }
+        return null;
+    }
+
+    @Override
+    public V put(K key, V value) {
+        var bucketIdx = getBucketIdx(key);
+        var bucket = buckets.get(bucketIdx);
+        if (bucket == null) {
+            bucket = new ArrayList<>();
+            buckets.set(bucketIdx, bucket);
+            bucket.add(new AbstractMap.SimpleEntry<>(key, value));
+            elementCount++;
+            rehashIfNeeded();
+            return null;
+        }
+        for (var entry : bucket) {
+            if (Objects.equals(entry.getKey(), key)) {
+                var oldValue = entry.getValue();
+                entry.setValue(value);
+                return oldValue;
+            }
+        }
+        bucket.add(new AbstractMap.SimpleEntry<>(key, value));
+        elementCount++;
+        rehashIfNeeded();
+        return null;
+    }
+
+    @Override
+    public V remove(Object key) {
+        var bucket = buckets.get(getBucketIdx(key));
+        if (bucket == null) {
+            return null;
+        }
+        for (var entry : bucket) {
+            if (Objects.equals(entry.getKey(), key)) {
+                var oldValue = entry.getValue();
+                bucket.remove(entry);
+                elementCount--;
+                rehashIfNeeded();
+                return oldValue;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public void putAll(@NotNull Map<? extends K, ? extends V> m) {
+        m.forEach(this::put);
+    }
+
+    @Override
+    public void clear() {
+        bucketCount = initialBucketCount;
+        elementCount = 0;
+        buckets = new ArrayList<>(bucketCount);
+    }
+
+    @Override
+    public @NotNull Set<K> keySet() {
+        return new AbstractSet<>() {
+            @Override
+            public Iterator<K> iterator() {
+                return entrySet().stream().map(Entry::getKey).iterator();
+            }
+
+            @Override
+            public int size() {
+                return DictionaryImpl.this.size();
+            }
+        };
+    }
+
+    @Override
+    public @NotNull Collection<V> values() {
+        return new AbstractCollection<>() {
+            @Override
+            public Iterator<V> iterator() {
+                return entrySet().stream().map(Entry::getValue).iterator();
+            }
+
+            @Override
+            public int size() {
+                return DictionaryImpl.this.size();
+            }
+        };
+    }
+
+    @Override
+    public @NotNull Set<Entry<K, V>> entrySet() {
+        return new AbstractSet<>() {
+            @Override
+            public Iterator<Entry<K, V>> iterator() {
+                return new Iterator<>() {
+
+                    private final Iterator<ArrayList<Entry<K, V>>> bucketIterator = buckets.iterator();
+                    private ArrayList<Entry<K, V>> currentBucket = null;
+                    private ArrayList<Entry<K, V>> nextBucket = null;
+                    private int c = 0;
+
+                    {
+                        while (bucketIterator.hasNext()) {
+                            currentBucket = bucketIterator.next();
+                            if (currentBucket != null) break;
+                        }
+
+                        while (bucketIterator.hasNext()) {
+                            nextBucket = bucketIterator.next();
+                            if (nextBucket != null) break;
+                        }
+                    }
+
+                    @Override
+                    public boolean hasNext() {
+                        return nextBucket != null || (currentBucket != null && c < currentBucket.size());
+                    }
+
+                    @Override
+                    public Entry<K, V> next() {
+                        if (currentBucket == null) {
+                            throw new NoSuchElementException();
+                        }
+                        if (c < currentBucket.size()) {
+                            c++;
+                        } else {
+                            c = 0;
+                            currentBucket = nextBucket;
+                            nextBucket = null;
+                            while (bucketIterator.hasNext()) {
+                                nextBucket = bucketIterator.next();
+                                if (nextBucket != null) break;
+                            }
+                        }
+                        return currentBucket.get(c);
+                    }
+                };
+            }
+
+            @Override
+            public int size() {
+                return DictionaryImpl.this.size();
+            }
+        };
+    }
+}
---
+++ src/test/ru/itmo/mit/util/DictionaryImplTests.java
@@ -0,0 +1,103 @@
+package ru.itmo.mit.util;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+@SuppressWarnings({"ConstantValue", "SameParameterValue"})
+public class DictionaryImplTests {
+
+    @SuppressWarnings("unused")
+    @Test
+    public void dictionaryCreateTest() {
+        var d1 = new DictionaryImpl<Integer, Integer>();
+        var d2 = new DictionaryImpl<Dictionary<String, String>, Integer>(0.5);
+        var d3 = new DictionaryImpl<Integer, String>(100);
+        var d4 = new DictionaryImpl<String, Double>(0.4, 30);
+    }
+
+    @Test
+    public void dictionaryBasicTest() {
+        var d = new DictionaryImpl<Integer, String>();
+        Assertions.assertEquals(0, d.size());
+        Assertions.assertTrue(d.isEmpty());
+
+        addWithAssertions(d, 10, "Hello");
+        addWithAssertions(d, 12, "World");
+
+        Assertions.assertFalse(d.isEmpty());
+
+        deleteNonExistingWithAssertions(d, 42);
+        deleteWithAssertions(d, "Hello", 10);
+
+        Assertions.assertTrue(d.containsKey(12));
+        Assertions.assertFalse(d.containsKey(10));
+        Assertions.assertFalse(d.containsKey(11));
+        Assertions.assertFalse(d.containsKey(42));
+
+        addWithAssertions(d, 10000, "Wow, that's a big number!");
+        Assertions.assertTrue(d.containsKey(10000));
+        Assertions.assertTrue(d.containsValue("Wow, that's a big number!"));
+        changeWithAssertions(d, "Wow, that's a big number!", 10000, "");
+        Assertions.assertTrue(d.containsKey(10000));
+        Assertions.assertFalse(d.containsValue("Wow, that's a big number!"));
+        Assertions.assertTrue(d.containsValue(""));
+
+        Assertions.assertEquals("World", d.get(12));
+        d.clear();
+        Assertions.assertEquals(0, d.size());
+        Assertions.assertTrue(d.isEmpty());
+    }
+
+    @Test
+    public void dictionaryNullTest() {
+        var d = new DictionaryImpl<Integer, String>();
+
+        addWithAssertions(d, null, "Help");
+        Assertions.assertTrue(d.containsKey(null));
+        Assertions.assertTrue(d.containsValue("Help"));
+
+        changeWithAssertions(d,"Help", null, "Ok, I'm helping");
+        Assertions.assertTrue(d.containsKey(null));
+        Assertions.assertTrue(d.containsValue("Ok, I'm helping"));
+        Assertions.assertFalse(d.containsValue("Help"));
+
+        deleteWithAssertions(d, "Ok, I'm helping", null);
+        Assertions.assertTrue(d.isEmpty());
+
+        Assertions.assertFalse(d.containsValue(null));
+        addWithAssertions(d, -100, null);
+        Assertions.assertTrue(d.containsValue(null));
+        changeWithAssertions(d, null, -100, "");
+        Assertions.assertFalse(d.containsValue(null));
+
+        Assertions.assertFalse(d.containsKey(null));
+        addWithAssertions(d, null, null);
+        Assertions.assertTrue(d.containsValue(null));
+        Assertions.assertTrue(d.containsKey(null));
+
+    }
+
+    private <K, V> void addWithAssertions(Dictionary<K, V> dict, K key, V value) {
+        var size = dict.size();
+        Assertions.assertNull(dict.put(key, value));
+        Assertions.assertEquals(size + 1, dict.size());
+    }
+
+    private <K, V> void changeWithAssertions(Dictionary<K, V> dict, V prev, K key, V value) {
+        var size = dict.size();
+        Assertions.assertEquals(prev, dict.put(key, value));
+        Assertions.assertEquals(size, dict.size());
+    }
+
+    private <K, V> void deleteWithAssertions(Dictionary<K, V> dict, V prev, K key) {
+        var size = dict.size();
+        Assertions.assertEquals(prev, dict.remove(key));
+        Assertions.assertEquals(size - 1, dict.size());
+    }
+
+    private <K, V> void deleteNonExistingWithAssertions(Dictionary<K, V> dict, K key) {
+        var size = dict.size();
+        Assertions.assertNull(dict.remove(key));
+        Assertions.assertEquals(size, dict.size());
+    }
+}
