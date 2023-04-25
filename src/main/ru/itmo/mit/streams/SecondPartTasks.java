a1deabbccc757af28a3b9e3e55182d4b8ae12f55
Андрей Егорычев <thelastmind@yandex.ru>
2023-04-25 13:36:43 +0300

    task 2

--- src/main/ru/itmo/mit/streams/SecondPartTasks.java
+++ src/main/ru/itmo/mit/streams/SecondPartTasks.java
@@ -1,16 +1,25 @@
 package ru.itmo.mit.streams;
 
-import java.io.IOException;
-import java.util.List;
-import java.util.Map;
+import java.io.*;
+import java.util.*;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public final class SecondPartTasks {
 
-    private SecondPartTasks() {}
+    private SecondPartTasks() {
+    }
 
     // Найти строки из переданных файлов, в которых встречается указанная подстрока.
     public static List<String> findQuotes(List<String> paths, CharSequence sequence) throws IOException {
-        throw new UnsupportedOperationException();
+        return paths.stream().map(name -> {
+            try {
+                return new BufferedReader(new FileReader(name));
+            } catch (FileNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+        }).flatMap(BufferedReader::lines).filter(x -> x.contains(sequence)).collect(Collectors.toList());
     }
 
     // В квадрат с длиной стороны 1 вписана мишень.
@@ -21,18 +30,24 @@ public final class SecondPartTasks {
     }
 
     public static double piDividedBy4(int n) {
-        throw new UnsupportedOperationException();
+        // not really sure if there is a way to do it in one line without two calls to nextDouble()
+        var r = new Random();
+        return (double) new Random().doubles(n)
+                .mapToObj(x -> new AbstractMap.SimpleEntry<>(x, r.nextDouble()))
+                .filter(x -> Math.hypot(x.getKey(), x.getValue()) <= 1d).count() / n;
     }
 
     // Дано отображение из имени автора в список с содержанием его произведений.
     // Надо вычислить, чья общая длина произведений наибольшая.
     public static String findPrinter(Map<String, List<String>> compositions) {
-        throw new UnsupportedOperationException();
+        return compositions.entrySet().stream()
+                .map(x -> new AbstractMap.SimpleEntry<>(x.getKey(), x.getValue().stream().mapToInt(String::length).sum()))
+                .max(Map.Entry.comparingByValue()).map(AbstractMap.SimpleEntry::getKey).orElse("");
     }
 
     // Вы крупный поставщик продуктов. Каждая торговая сеть делает вам заказ в виде Map<Товар, Количество>.
     // Необходимо вычислить, какой товар и в каком количестве надо поставить.
     public static Map<String, Integer> calculateGlobalOrder(List<Map<String, Integer>> orders) {
-        throw new UnsupportedOperationException();
+        return orders.stream().flatMap(x -> x.entrySet().stream()).collect(Collectors.groupingBy(Map.Entry::getKey, Collectors.summingInt(Map.Entry::getValue)));
     }
 }
