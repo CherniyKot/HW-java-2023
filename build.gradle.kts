7b4fb24507282e65df86d870a188f4d31755b268
Nikolay Egorov <nikolay.egorov@jetbrains.com>
2023-05-01 16:50:15 +0300

    Add Git task description

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,10 @@ repositories {
 
 dependencies {
     implementation("org.jetbrains:annotations:16.0.2")
+    implementation("commons-codec:commons-codec:1.13") // утилиты для хеширования
+    implementation("commons-io:commons-io:2.7") // утилиты для работы с IO
+    implementation("commons-cli:commons-cli:1.5.0") // фреймворк для создания CLI
+
     testImplementation(platform("org.junit:junit-bom:5.7.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
@@ -24,6 +28,7 @@ java {
         }
         test {
             java.setSrcDirs(listOf("src/test"))
+            resources.setSrcDirs(listOf("src/testResources"))
         }
     }
 }
